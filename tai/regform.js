df1189c2d04560acbc15186ad87a8d55a75a8ccb
TAI Lab <33939191+ladooniani@users.noreply.github.com>
2021-01-05 21:11:04 +0400

    Update regform.js

--- tai/regform.js
+++ tai/regform.js
@@ -666,12 +666,15 @@ function sqNonAct(x){
 	}       
 }
 
-const fileUrl = 'https://missingmonument.com/temp/log.txt'
-
-readTextFromUrl();
-function readTextFromUrl(){
-
-fetch(fileUrl)
-   .then( r => r.text() )
-   .then( t => console.log(t) )
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", "https://missingmonument.com/temp/log.txt", true);
+txtFile.onreadystatechange = function() {
+  if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (txtFile.status === 200) {  // Makes sure it's found the file.
+      allText = txtFile.responseText;
+      lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+      console.log(lines)
+    }
+  }
 }
+txtFile.send(null);
