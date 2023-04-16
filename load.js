8d4333c185940d35a9abc2624b513f6e06da70c2
Lado Oniani <ladooniani@gmail.com>
2023-04-16 17:16:18 +0400

    Update load.js

--- load.js
+++ load.js
@@ -1,12 +1,17 @@
 function callLambda() {
-  console.log("Call lambda function");
-  fetch("https://d3s1nj7gz0.execute-api.eu-north-1.amazonaws.com/beta-test", {
-    method: "POST",
-    body: JSON.stringify({ number: 1 }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      alert(data.result);
-    });
+  const myButton = document.getElementById("myButton");
+  const response = document.getElementById("response");
+  myButton.addEventListener("click", () => {
+    console.log("Call Lambda function");
+    fetch("https://d3s1nj7gz0.execute-api.eu-north-1.amazonaws.com/beta-test")
+      .then((response) => response.json())
+      .then((data) => {
+        response.innerHTML = data;
+        console.log(data);
+        alert(data.result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 }
