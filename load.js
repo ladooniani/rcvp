e46808afc5b437324b8ebedfb404eb3393ce8151
Lado Oniani <ladooniani@gmail.com>
2023-04-16 17:26:57 +0400

    Update load.js

--- load.js
+++ load.js
@@ -1,17 +1,32 @@
 function callLambda() {
-  const myButton = document.getElementById("myButton");
   const response = document.getElementById("response");
-  myButton.addEventListener("click", () => {
-    console.log("Call Lambda function");
-    fetch("https://d3s1nj7gz0.execute-api.eu-north-1.amazonaws.com/beta-test")
-      .then((response) => response.json())
-      .then((data) => {
-        response.innerHTML = data;
-        console.log(data);
-        alert(data.result);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+  console.log("Call lambda function");
+  fetch("https://d3s1nj7gz0.execute-api.eu-north-1.amazonaws.com/beta-test", {
+    method: "POST",
+    body: JSON.stringify({ number: 1 }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      response.innerHTML = data;
+      console.log(data);
+      alert(data.result);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function callLambda0() {
+  const response = document.getElementById("response");
+  console.log("Call Lambda function");
+  fetch("https://d3s1nj7gz0.execute-api.eu-north-1.amazonaws.com/beta-test")
+    .then((response) => response.json())
+    .then((data) => {
+      response.innerHTML = data;
+      console.log(data);
+      alert(data.result);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
