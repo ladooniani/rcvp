3b545ddf710b51ced9ecb9692c49af6565c85d5d
Lado Oniani <ladooniani@gmail.com>
2023-06-26 05:55:09 +0400

    Update date.js

--- js/date.js
+++ js/date.js
@@ -2,6 +2,6 @@ getDate();
 function getDate() {
   var d = new Date();
   var n = d.getFullYear();
-  var cpr = "Copyright &copy; " + n + " Lado Oniani Software Research";
+  var cpr = "Copyright &copy; " + n + " Lado Oniani Software Research. All Rights Reserved.";
   document.getElementById("cr").innerHTML = cpr;
 }
